7e4401f93a18fe9cf836a78a06f44efb106f9ddf
Maarten van Ee <maartenvanee@gmail.com>
2019-09-19 14:53:00 +0200

    create

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Header/>
+  </div>
+</template>
+
+<script>
+import Header from './components/header/Header.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/header/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header"><h1>Rick and Morty</h1></div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+.header {
+  display: block;
+  width: 100%;
+  background-color: rgb(232, 232, 238);
+  padding: 30px 40px;
+
+  h1 {
+    font-size: 30px;
+  }
+}
+
+</style>
