1416bd658170770b56ce305b3a86df7107f3c1c6
Maarten van Ee <maartenvanee@gmail.com>
2019-10-17 12:09:32 +0200

    SingleCharacter

--- src/components/characters/SingleCharacter.vue
+++ src/components/characters/SingleCharacter.vue
@@ -1,9 +1,32 @@
 <template>
-  <h1>Single character: {{ $route.params.id }}</h1>
+  <div v-if="character">
+    <h1>{{ character.name }}</h1>
+    <img v-bind:src="character.image" />
+    <p>Status: {{character.status}}</p>
+    <p>Species: {{character.species}}</p>
+    <p>Gender: {{character.gender}}</p>
+    <p>Origin: {{character.origin.name}}</p>
+    <p>Current location: {{character.location.name}}</p>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "SingleCharacter"
+  name: "SingleCharacter",
+  data() {
+    return {
+      character: null,
+      characterUrl: "https://rickandmortyapi.com/api/character/"
+    };
+  },
+  mounted() {
+    axios.get(this.characterUrl + this.$route.params.id).then(response => {
+      if (response.data) {
+        this.character = response.data;
+      }
+    });
+  }
 };
 </script>
